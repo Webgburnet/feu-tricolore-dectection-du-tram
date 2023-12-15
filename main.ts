5419cb078d7790a544839b77115205e1574f6f34
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2023-12-15 14:11:19 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let Mesure_Distance = 0
+radio.setGroup(1)
 basic.forever(function () {
-	
+    Mesure_Distance = grove.measureInCentimeters(DigitalPin.P0)
+    if (Mesure_Distance <= 2) {
+        radio.sendString("Prioritaire")
+    }
 })
